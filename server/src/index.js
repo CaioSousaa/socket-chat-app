02ef028606ab708f3709b9eeb40c2fe1359d19fe
caio <caiothalles607@gmail.com>
2024-05-10 16:09:24 -0300

    feat: connected to MongoDb

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors);
+
+mongoose.connect(
+  "mongodb+srv://mongo:12345@cluster0.spphilp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 
 app.listen(5000, () => console.log("Server is run"));
