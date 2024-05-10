3c8cd534bc4152167c7d6f2ca0fdc5ede2119edb
caio <caiothalles607@gmail.com>
2024-05-10 17:06:13 -0300

    feat: create routeUser created

--- server/src/index.js
+++ server/src/index.js
@@ -2,16 +2,18 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("../src/routes/userRoutes");
 
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors);
+app.use(cors());
+app.use("/api/users", userRoutes);
 
 mongoose.connect(
   "mongodb+srv://mongo:12345@cluster0.spphilp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 );
 
-app.listen(5000, () => console.log("Server is run"));
+app.listen(3333, () => console.log("Server is run"));
