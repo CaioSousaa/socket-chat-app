863bfa00e21fc6bfa7653659224a7dbaf12b7729
caio <caiothalles607@gmail.com>
2024-05-11 15:47:37 -0300

    feat: created potential chats

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { baseUrl, getRequest, postRequest } from "../utils/service";
 
 export const ChatContext = createContext();
@@ -9,6 +10,35 @@ export const ChatContextProvider = ({ children, user }) => {
   const [userChats, setUserChats] = useState(null);
   const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
   const [userChatsError, setUserChatsError] = useState(null);
+  const [potentialChats, setPotentialChats] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await getRequest(`${baseUrl}/users`);
+
+      if (response.error) {
+        return console.log("Error fetching users", response);
+      }
+
+      const pChats = response.filter((u) => {
+        let isChatCreated = false;
+
+        if (user?._id === u._id) return false;
+
+        if (userChats) {
+          isChatCreated = userChats?.some((chat) => {
+            return chat.members[0] === u._id || chat.members[1] === u._id;
+          });
+        }
+
+        return !isChatCreated;
+      });
+
+      setPotentialChats(pChats);
+    };
+
+    getUsers();
+  }, [userChats]);
 
   useEffect(() => {
     const getUserChats = async () => {
@@ -30,12 +60,26 @@ export const ChatContextProvider = ({ children, user }) => {
     getUserChats();
   }, [user]);
 
+  const createChat = useCallback(async (firstId, secondId) => {
+    const response = await postRequest(
+      `${baseUrl}/chats`,
+      JSON.stringify({ firstId, secondId })
+    );
+    if (response.error) {
+      return console.log("Error creating chat", response);
+    }
+
+    setUserChats((prev) => [...prev, response]);
+  }, []);
+
   return (
     <ChatContext.Provider
       value={{
         userChats,
         isUserChatsLoading,
         userChatsError,
+        potentialChats,
+        createChat,
       }}
     >
       {children}
