2d4ba1bcb0ee530a772ac8d7b83c231c84bc1951
caio <caiothalles607@gmail.com>
2024-05-13 11:05:02 -0300

    feat: created chat box

--- client/src/hooks/useFetchRecipient.js
+++ client/src/hooks/useFetchRecipient.js
@@ -22,7 +22,7 @@ export const useFetchRecipientUser = (chat, user) => {
       setRecipientUser(response);
     };
     getUsers();
-  }, []);
+  }, [recipientId]);
 
   return { recipientUser };
 };
